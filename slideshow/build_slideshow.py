de57956f91833715579dd8dfa2a3867a35a0ee94
Martin Cech <cech.marten@gmail.com>
2016-10-07 15:04:55 -0400

    make slides to be built in common dir

--- slideshow/build_slideshow.py
+++ slideshow/build_slideshow.py
@@ -7,20 +7,20 @@ SCRIPTS_DIRECTORY = os.path.dirname(__file__)
 TEMPLATE_PATH = os.path.join(SCRIPTS_DIRECTORY, "slideshow_template.html")
 TEMPLATE = string.Template(open(TEMPLATE_PATH, "r").read())
 
-
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     title = argv[1]
     markdown_source = argv[2]
-    output = os.path.splitext(markdown_source)[0] + '.html'
+    build_dir = argv[3]
+    head, tail = os.path.split(markdown_source)
+    output = os.path.join(build_dir, os.path.splitext(tail)[0] + '.html')
     with open(markdown_source, "r") as s:
         content = s.read()
     html = TEMPLATE.safe_substitute(**{
         'title': title,
         'content': content,
     })
-    print html
     open(output, "w").write(html)
 
 if __name__ == "__main__":
