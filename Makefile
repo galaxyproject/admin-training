56a3c4288423ea4597d36143266a3afe092f31c4
Martin Cech <cech.marten@gmail.com>
2016-10-20 10:43:16 -0400

    adjust makefile target to build the githubpage index

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@ VENV?=../galaxy/.venv
 IN_VENV=if [ -f $(VENV)/bin/activate ]; then . $(VENV)/bin/activate; fi;
 
 generate-slides:
-	$(IN_VENV) python slideshow/build_slideshow.py 'Tool Shed Intro' intro/05-tool-shed/shed_intro.md docs/05-tool-shed
-	$(IN_VENV) python slideshow/build_slideshow.py 'Galaxy Architecture' intro/12-architecture/galaxy_architecture.md docs/12-architecture
+	rm docs/index.html
+	echo "<html><head></head><body>" > docs/index.html
+	$(IN_VENV) python slideshow/build_slideshow.py 'Tool Shed Intro' intro/05-tool-shed/shed_intro.md 05-tool-shed
+	$(IN_VENV) python slideshow/build_slideshow.py 'Galaxy Architecture' intro/12-architecture/galaxy_architecture.md 12-architecture
+	echo "</body></html>" >> docs/index.html
--- slideshow/build_slideshow.py
+++ slideshow/build_slideshow.py
@@ -6,6 +6,7 @@ import sys
 SCRIPTS_DIRECTORY = os.path.dirname(__file__)
 TEMPLATE_PATH = os.path.join(SCRIPTS_DIRECTORY, "slideshow_template.html")
 TEMPLATE = string.Template(open(TEMPLATE_PATH, "r").read())
+BUILD_PATH = 'docs'
 
 def main(argv=None):
     if argv is None:
@@ -13,9 +14,10 @@ def main(argv=None):
     title = argv[1]
     markdown_source = argv[2]
     build_dir = argv[3]
+    full_build_dir = os.path.join(BUILD_PATH, build_dir)
     head, tail = os.path.split(markdown_source)
-    output = os.path.join(build_dir, os.path.splitext(tail)[0] + '.html')
-    print output
+    filename = os.path.splitext(tail)[0] + '.html'
+    output = os.path.join(full_build_dir, filename)
     with open(markdown_source, "r") as s:
         content = s.read()
     html = TEMPLATE.safe_substitute(**{
@@ -23,6 +25,11 @@ def main(argv=None):
         'content': content,
     })
     open(output, "w").write(html)
+
+    # Add the the entry to index.html.
+    slidepath = build_dir + '/' + filename
+    open( BUILD_PATH + '/index.html', "a").write('<a href="'+ slidepath +'">' + slidepath + '</a>\n')
+
 
 if __name__ == "__main__":
     main()
