d32e9bed46befe785b49d2a02d60c66190e98c39
Helena Rasche <hxr@hx42.org>
2021-06-25 18:25:33 +0200

    test wf to prep remotely

--- terraform/process-training-output.sh
+++ terraform/process-training-output.sh
@@ -4,7 +4,6 @@ if (( $# > 0 )); then
 fi
 
 ips="$(terraform output -json | jq -r '."training_ips'$DOMAIN'".value[0][]')"
-passwords="$(terraform output -json | jq -r '."training_pws'$DOMAIN'".value[0][]')"
 dns="$(terraform output -json | jq -r '."training_dns'$DOMAIN'".value[0][]')"
 lines=$(echo "$ips" | wc -l)
 user="$(yes ubuntu | head -n $lines)"
