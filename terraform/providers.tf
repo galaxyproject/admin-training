5257e1ea8fd30ff2f4aa83009c2513b6f630f404
Helena Rasche <hxr@hx42.org>
2022-03-07 14:52:28 +0100

    cleanup, ready to apply

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,44 +1,42 @@
-variable "VSC_OS_APPLICATION_CREDENTIAL_ID" {
-  type        = string
-  description = "OS_APPLICATION_CREDENTIAL_ID"
+provider "aws" {
 }
 
-variable "VSC_OS_APPLICATION_CREDENTIAL_SECRET" {
-  type        = string
-  description = "OS_APPLICATION_CREDENTIAL_SECRET"
+variable "zone_galaxyproject_eu" {
+  default = "Z386N8B8JBC6TQ"
 }
 
-variable "EU_OS_USERNAME" {
-  type        = string
-  description = "OS_USERNAME"
+variable "zone_usegalaxy_eu" {
+  default = "Z1C7L7XFF9613J"
 }
 
-variable "EU_OS_PASSWORD" {
-  type        = string
-  description = "OS_PASSWORD"
+data "aws_route53_zone" "training-gxp-eu" {
+  name         = "training.galaxyproject.eu."
 }
 
-provider "openstack" {
-  auth_url = "https://localhost"
-  region   = "regionOne"
-}
 
-provider "openstack" {
-  alias                         = "vsc"
-  auth_url                      = "https://cloud.vscentrum.be:13000"
-  region                        = "regionOne"
-  application_credential_id     = var.VSC_OS_APPLICATION_CREDENTIAL_ID
-  application_credential_secret = var.VSC_OS_APPLICATION_CREDENTIAL_SECRET
-}
+#variable "EU_OS_USERNAME" {
+#type        = string
+#description = "OS_USERNAME"
+#}
 
-provider "openstack" {
-  alias               = "eu"
-  auth_url            = "https://idm01.bw-cloud.org:5000/v3"
-  project_domain_name = "Default"
-  tenant_name         = "freiburg_galaxy"
-  region              = "Freiburg"
+#variable "EU_OS_PASSWORD" {
+#type        = string
+#description = "OS_PASSWORD"
+#}
+
+#provider "openstack" {
+#auth_url = "https://localhost"
+#region   = "regionOne"
+#}
+
+#provider "openstack" {
+#alias               = "eu"
+#auth_url            = "https://idm01.bw-cloud.org:5000/v3"
+#project_domain_name = "Default"
+#tenant_name         = "freiburg_galaxy"
+#region              = "Freiburg"
 
 
-  user_name = var.EU_OS_USERNAME
-  password  = var.EU_OS_PASSWORD
-}
+#user_name = var.EU_OS_USERNAME
+#password  = var.EU_OS_PASSWORD
+#}
