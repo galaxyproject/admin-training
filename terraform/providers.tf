8d05e708543fe7f92801ddb3d4ee861e819c2d2b
Helena Rasche <hxr@hx42.org>
2022-03-08 18:01:21 +0100

    swap out zone id autodetection

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,20 @@
 provider "aws" {
 }
 
-data "aws_route53_zone" "training-gxp-oz" {
-  name = "oz.galaxy.training."
+#data "aws_route53_zone" "training-gxp-oz" {
+  #name = "oz.galaxy.training."
+#}
+
+#data "aws_route53_zone" "training-gxp-us" {
+  #name = "us.galaxy.training."
+#}
+
+variable "training-gxp-oz" {
+  type = string
+  default = 'Z06074271TNBSU75H5S3Y'
 }
 
-data "aws_route53_zone" "training-gxp-us" {
-  name = "us.galaxy.training."
+variable "training-gxp-us" {
+  type = string
+  default = 'Z022528316NCQCRTGOOLK'
 }
