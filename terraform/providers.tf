dc0aed8c44f7578fb19d26d83d7f26d17d6eef9b
Helena Rasche <hxr@hx42.org>
2022-03-08 17:49:39 +0100

    move everything under galaxy.training

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,42 +1,10 @@
 provider "aws" {
 }
 
-variable "zone_galaxyproject_eu" {
-  default = "Z386N8B8JBC6TQ"
+data "aws_route53_zone" "training-gxp-oz" {
+  name = "oz.galaxy.training."
 }
 
-variable "zone_usegalaxy_eu" {
-  default = "Z1C7L7XFF9613J"
+data "aws_route53_zone" "training-gxp-us" {
+  name = "us.galaxy.training."
 }
-
-data "aws_route53_zone" "training-gxp-eu" {
-  name = "training.galaxyproject.eu."
-}
-
-
-#variable "EU_OS_USERNAME" {
-#type        = string
-#description = "OS_USERNAME"
-#}
-
-#variable "EU_OS_PASSWORD" {
-#type        = string
-#description = "OS_PASSWORD"
-#}
-
-#provider "openstack" {
-#auth_url = "https://localhost"
-#region   = "regionOne"
-#}
-
-#provider "openstack" {
-#alias               = "eu"
-#auth_url            = "https://idm01.bw-cloud.org:5000/v3"
-#project_domain_name = "Default"
-#tenant_name         = "freiburg_galaxy"
-#region              = "Freiburg"
-
-
-#user_name = var.EU_OS_USERNAME
-#password  = var.EU_OS_PASSWORD
-#}
