9b1f5e699213b787461e4ab1abf468e6e9c9ccb0
Mira Kuntz <mira.kuntz@yahoo.com>
2023-04-05 17:43:26 +0200

    cleanup

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,18 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-variable "training-gxp-oz" {
-  type    = string
-  default = "Z06074271TNBSU75H5S3Y"
-}
-
-variable "training-gxp-us" {
-  type    = string
-  default = "Z022528316NCQCRTGOOLK"
-}
-
-variable "training-gxp-eu" {
-  type    = string
-  default = "Z061285412X3TSRV2I2PH"
-}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,5 +1,14 @@
+variable "training-gxp-oz" {
+  type    = string
+  default = "Z06074271TNBSU75H5S3Y"
+}
+
+variable "training-gxp-us" {
+  type    = string
+  default = "Z022528316NCQCRTGOOLK"
+}
 
 variable "training-gxp-eu" {
-  default = "Z05016927AMHTHGB1IS2"
-  #name         = "training.galaxyproject.eu"
+  type    = string
+  default = "Z061285412X3TSRV2I2PH"
 }
